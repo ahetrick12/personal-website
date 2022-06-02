d4896831fdcc7e8bd3a3e56f3310fd427e70aaa2
Alex Hetrick <ventuargames@gmail.com>
2022-06-01 23:16:42 -0400

    Reverted back to temporary webpage

--- src/index.js
+++ src/index.js
@@ -6,10 +6,8 @@ import reportWebVitals from "./reportWebVitals";
 import "bulma/css/bulma.min.css";
 
 ReactDOM.render(
-	<React.StrictMode>
-		<App />
-	</React.StrictMode>,
-	document.getElementById("root")
+	<React.StrictMode></React.StrictMode>
+	//document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
