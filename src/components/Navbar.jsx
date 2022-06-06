82acb221bd15871c75461004de41db9d128d6797
Alex Hetrick <ventuargames@gmail.com>
2022-06-06 18:16:56 -0400

    Adjusted navbar formatting

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,37 +5,39 @@ const Navbar = () => {
 	const [isActive, setIsActive] = useState(false);
 
 	return (
-		<nav
-			className="navbar is-fixed-top"
-			role="navigation"
-			aria-label="main navigation"
-		>
-			<div className="navbar-brand">
-				<img src={Logo} alt="logo" className="image is-64x64 m-2" />
-				<button
-					onClick={() => {
-						setIsActive(!isActive);
-					}}
-					class={`navbar-burger ${isActive ? "is-active" : ""}`}
-					aria-label="menu"
-					aria-expanded="false"
-				>
-					<span aria-hidden="true"></span>
-					<span aria-hidden="true"></span>
-					<span aria-hidden="true"></span>
-				</button>
-			</div>
-			<div className={`navbar-menu ${isActive ? "is-active" : ""}`}>
-				<div
-					className="navbar-start"
-					style={{ "flex-grow": 1, "justify-content": "center" }}
-				>
-					<p className="navbar-item">Home</p>
-					<p className="navbar-item">About</p>
-					<p className="navbar-item">Projects</p>
+		<section class="section">
+			<nav
+				className="navbar is-transparent is-fixed-top "
+				role="navigation"
+				aria-label="main navigation"
+			>
+				<div className="navbar-brand">
+					<img src={Logo} alt="logo" className="image is-64x64 m-2" />
+					<button
+						onClick={() => {
+							setIsActive(!isActive);
+						}}
+						class={`navbar-burger ${isActive ? "is-active" : ""}`}
+						aria-label="menu"
+						aria-expanded="false"
+					>
+						<span aria-hidden="true"></span>
+						<span aria-hidden="true"></span>
+						<span aria-hidden="true"></span>
+					</button>
 				</div>
-			</div>
-		</nav>
+				<div className={`navbar-menu ${isActive ? "is-active" : ""}`}>
+					<div
+						className="navbar-end"
+						style={{ "flex-grow": 1, "justify-content": "center" }}
+					>
+						<p className="navbar-item">Home</p>
+						<p className="navbar-item">About</p>
+						<p className="navbar-item">Projects</p>
+					</div>
+				</div>
+			</nav>
+		</section>
 	);
 };
 
