4969f811832a4940986b8b2c8a068d3bcb10fdc4
Alex Hetrick <ventuargames@gmail.com>
2022-06-14 15:42:59 -0400

    Added AOS back into about page

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -2,12 +2,14 @@ import Logo from "../images/logo.png";
 
 const About = () => {
 	return (
-		<section id="about" className="section is-medium">
+		<section id="about">
 			<div className="about-container">
-				<h2 className="title">About</h2>
+				<h2 className="title" data-aos="fade-up">
+					About
+				</h2>
 				<div className="description columns mx-0">
 					<div className="column is-5 m-auto p-6">
-						<p>
+						<p data-aos="fade-right">
 							Hi there! I’m Alex Hetrick, a developer and artist
 							from Pittsburgh, Pennsylvania. I’m currently a
 							second-year student at the Rochester Institute of
@@ -19,11 +21,12 @@ const About = () => {
 							use in their projects.
 						</p>
 					</div>
-					<figure class="image p-6 m-auto column is-3">
+					<figure class="image p-6 m-auto column is-3 py-0">
 						<img
 							className="standard-radius"
 							src={Logo}
 							alt="Alex"
+							data-aos="fade-left"
 						/>
 					</figure>
 				</div>
