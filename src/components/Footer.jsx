1642ab198f973291cba20b533ecc00930f65405f
Alex Hetrick <ventuargames@gmail.com>
2022-08-05 21:10:07 -0400

    Modified footer styling

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,24 +2,37 @@ const Footer = () => {
 	return (
 		<footer id="footer">
 			<div className="content has-text-centered">
-				<p>
-					Powered using{" "}
-					<a href="https://bulma.io" target="_blank" rel="noreferrer">
-						Bulma
-					</a>{" "}
-					and{" "}
-					<a
-						href="https://reactjs.org"
-						target="_blank"
-						rel="noreferrer"
-					>
-						React
-					</a>
-					.
-					<br />
-					Designed and developed by Alex Hetrick | © 2022 <br />
-					All Rights Reserved.
-				</p>
+				<hr
+					className="primary"
+					data-aos="lengthen"
+					data-aos-duration="1300"
+				/>
+				<div className="columns">
+					<div className="column is-one-quarter" />
+					<p className="column is-half">
+						Powered using{" "}
+						<a
+							href="https://bulma.io"
+							target="_blank"
+							rel="noreferrer"
+						>
+							Bulma
+						</a>{" "}
+						and{" "}
+						<a
+							href="https://reactjs.org"
+							target="_blank"
+							rel="noreferrer"
+						>
+							React
+						</a>
+						.
+						<br />
+						Designed and developed by Alex Hetrick | © 2022 <br />
+						All Rights Reserved.
+					</p>
+					<div className="column is-one-quarter links">hi there</div>
+				</div>
 			</div>
 		</footer>
 	);
