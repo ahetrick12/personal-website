50ad5388a2304162e14e6a0b31ef9e79d252b14c
Alex Hetrick <aph1777@rit.edu>
2022-10-28 23:15:48 -0400

    Artwork optimization and dataset population

--- src/components/ArtworkDataset.jsx
+++ src/components/ArtworkDataset.jsx
@@ -5,7 +5,7 @@ function ArtworkDataset() {
 		{
 			image: images.MountainForest,
 			title: "Mountain Forest",
-			description: "",
+			description: "A warm forest scene with a mountain in the distance.",
 			link: "",
 		},
 		{
@@ -24,187 +24,205 @@ function ArtworkDataset() {
 		{
 			image: images.ShipDeck,
 			title: "Low Poly Underwater Pack",
-			description: "A shot of the ship deck",
+			description: "A scene of a ship deck in the middle of the ocean.",
 			link: "",
 		},
 		{
 			image: images.Corals1,
 			title: "Low Poly Underwater Pack",
-			description: "",
+			description: "An underwater scene with some corals.",
 			link: "",
 		},
 		{
 			image: images.Corals2,
-			title: "Mountain Forest",
-			description: "",
+			title: "Low Poly Underwater Pack",
+			description: "Some corals and fish under the ocean.",
 			link: "",
 		},
 		{
 			image: images.SunkenShip,
-			title: "Mountain Forest",
-			description: "",
+			title: "Low Poly Underwater Pack",
+			description: "An underwater sunken ship, torn in two.",
 			link: "",
 		},
 		{
 			image: images.InsideSunkenShip,
-			title: "Mountain Forest",
-			description: "",
+			title: "Low Poly Underwater Pack",
+			description: "Inside the underwater sunken ship.",
 			link: "",
 		},
 		{
 			image: images.Corals3,
 			title: "Mountain Forest",
-			description: "",
+			description:
+				"An underwater scene of corals with a sunken ship in the distance.",
 			link: "",
 		},
 		{
 			image: images.LogCabin,
-			title: "Mountain Forest",
-			description: "",
+			title: "Holiday Log Cabin",
+			description:
+				"A cozy log cabin in the woods during the cold winter months.",
 			link: "",
 		},
 		{
 			image: images.Jim2,
-			title: "Mountain Forest",
-			description: "",
+			title: "Cel-Shaded Jim",
+			description:
+				"Jim, sporting a fancy scarf and a stylish custom-build cel shader. He thinks shirts hold back.",
 			link: "",
 		},
 		{
 			image: images.Skyscraper,
-			title: "Mountain Forest",
-			description: "",
+			title: "Nighttime Skyscraper",
+			description:
+				"A sky-view nighttime scene of a skyscraper peaking above the clouds",
 			link: "",
 		},
 		{
 			image: images.Ruins,
-			title: "Mountain Forest",
-			description: "",
+			title: "Sunset Ruins",
+			description: "Some stone ruins with a sunset in the background.",
 			link: "",
 		},
 		{
 			image: images.Alley,
-			title: "Mountain Forest",
-			description: "",
+			title: "Dark Alleyway",
+			description: "A dark, dimly lit city alleyway.",
 			link: "",
 		},
 		{
 			image: images.PineForest,
-			title: "Mountain Forest",
-			description: "",
+			title: "Pine Forest",
+			description:
+				"A warm, pine forest with a small stream running through it.",
 			link: "",
 		},
 		{
 			image: images.CastleOverview1,
-			title: "Mountain Forest",
-			description: "",
+			title: "Low Poly Medieval Castle Pack",
+			description:
+				"An island with a forest and a windmill, with a castle showing in the background.",
 			link: "",
 		},
 		{
 			image: images.CastleBackBridge,
-			title: "Mountain Forest",
-			description: "",
+			title: "Low Poly Medieval Castle Pack",
+			description:
+				"An evening scene of the backside drawbridge of a castle.",
 			link: "",
 		},
 		{
 			image: images.CastleChurch,
-			title: "Mountain Forest",
-			description: "",
+			title: "Low Poly Medieval Castle Pack",
+			description:
+				"The castle's church and graveyard, with light shining out the windows.",
 			link: "",
 		},
 		{
 			image: images.CastleCity,
-			title: "Mountain Forest",
-			description: "",
+			title: "Low Poly Medieval Castle Pack",
+			description: "The inner city of a castle.",
 			link: "",
 		},
 		{
 			image: images.CastleOverview2,
-			title: "Mountain Forest",
-			description: "",
+			title: "Low Poly Medieval Castle Pack",
+			description: "An overview scene of a castle with a forest outside.",
 			link: "",
 		},
 		{
 			image: images.SmallCastleTown,
-			title: "Mountain Forest",
-			description: "",
+			title: "Low Poly Medieval Castle Pack",
+			description: "A small town next to a lake.",
 			link: "",
 		},
 		{
 			image: images.RiverForest,
-			title: "Mountain Forest",
-			description: "",
+			title: "Forest River",
+			description: "A forest with a river running through it at sunset.",
 			link: "",
 		},
 		{
 			image: images.RogueKnight,
-			title: "Mountain Forest",
-			description: "",
+			title: "Rogue Knight",
+			description:
+				"An armored rogue night with a broadsword strapped to his back.",
 			link: "",
 		},
 		{
 			image: images.Face,
-			title: "Mountain Forest",
-			description: "",
+			title: "Head Bust",
+			description:
+				"An attempt to construct a head and face from scratch.",
 			link: "",
 		},
 		{
 			image: images.LivingRoom,
-			title: "Mountain Forest",
-			description: "",
+			title: "Living Room",
+			description: "A warm, cozy, lived-in living room.",
 			link: "",
 		},
 		{
 			image: images.Dungeon,
-			title: "Mountain Forest",
-			description: "",
+			title: "Throne Room",
+			description: "A mysterious, omunious, fire-lit throne room.",
 			link: "",
 		},
 		{
 			image: images.Italy,
-			title: "Mountain Forest",
-			description: "",
+			title: "Italian Lakeside",
+			description:
+				"A small Italian town along the lake with the setting sun in the background.",
 			link: "",
 		},
 		{
 			image: images.FlailCharacters,
-			title: "Mountain Forest",
-			description: "",
+			title: "Low Poly Characters",
+			description:
+				"A few medieval-fantasy styled characters I made for a game, unofficially titled Project Flail.",
 			link: "",
 		},
 		{
 			image: images.AutumnBridge,
-			title: "Mountain Forest",
-			description: "",
+			title: "Autumn Bridge",
+			description:
+				"A bridge over a river during autumn, with trees and falling leaves in the background.",
 			link: "",
 		},
 		{
 			image: images.Odin,
-			title: "Mountain Forest",
-			description: "",
+			title: "Diety Soul + Constructs",
+			description:
+				'A character made for a modeling challenge with the dual theme of diety soul and "constructs."',
 			link: "",
 		},
 		{
 			image: images.Dream,
-			title: "Mountain Forest",
-			description: "",
+			title: "Sensory Field + Dream",
+			description:
+				'A character made for a modeling challenge with the dual theme of diety soul and "dream."',
 			link: "",
 		},
 		{
 			image: images.Mech,
-			title: "Mountain Forest",
-			description: "",
+			title: "Mech",
+			description:
+				"A bipedal mech with guns for hands and a rifle attached to its back.",
 			link: "",
 		},
 		{
 			image: images.Monk,
-			title: "Mountain Forest",
-			description: "",
+			title: "Fantasy Monk",
+			description:
+				"A cool looking monk with a neat hat and weird glowy orbs of energy or something.",
 			link: "",
 		},
 		{
 			image: images.CavernSword,
-			title: "Mountain Forest",
-			description: "",
+			title: "Cavern Sword",
+			description:
+				"A dungeon-like cavern with a glowing sword stuck a rock in the middle of a chasm.",
 			link: "",
 		},
 	];
